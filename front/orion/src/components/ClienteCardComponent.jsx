8ccb805f401fa63fa315efa940d2c1cacb6032e7
Matheus Mikio <matheusmikio15@gmail.com>
2025-07-07 03:49:51 -0300

    Create GET dos clientes

--- front/orion/src/components/ClienteCardComponent.jsx
+++ front/orion/src/components/ClienteCardComponent.jsx
@@ -2,14 +2,20 @@ import styles from "./ClientCard.module.css"
 import LinkButton from "./layout/LinkButton"
 
 export default function ClientCardComponent({client}){
+
+    const formatCPF = (cpf) => {
+        const cleaned = cpf.replace(/\D/g, ""); 
+        return cleaned.replace(/(\d{3})(\d{3})(\d{3})(\d{2})/, "$1.$2.$3-$4");
+    };
+
     return(
         <div className={styles.card}>
             <ul>
                 <li><span>Nome:</span> {client.nome}</li>
-                <li><span>CPF:</span> {client.cpf}</li>
+                <li><span>CPF:</span> {formatCPF(client.cpf)}</li>
                 <li><span>Email:</span> {client.email}</li>
                 <li><span>Idade:</span> {client.idade}</li>
-                <li><span>Valor Total:</span> {client.totalDivida.toLocaleString('pt-BR',{ style : 'currency', currency: 'BRL'})}</li>
+                <li><span>Valor Total:</span> {client.dividas.reduce((total, divida) => total + divida.valor, 0).toLocaleString('pt-BR',{ style: 'currency', currency: 'BRL'})}</li>
             </ul>
             <div>
                 <LinkButton text="VER MAIS" to={`${client.id}`} customClass={styles.btn}/>
--- front/orion/src/services/clienteService.js
+++ front/orion/src/services/clienteService.js
@@ -1,5 +1,59 @@
 const urlApi = "https://localhost:7214";
 
+
+export async function getClients(pesquisa){
+  try{
+    const response = await fetch(`${urlApi}/api/Cliente?pesquisa=${pesquisa || ""}`, {
+      method: "GET"
+    });
+
+    let dados;
+    if (response.status === 200) dados = await response.json()
+    
+    return {
+      status: response.status,
+      data: dados
+    }
+  }
+  catch{
+    return{
+      status: 500,
+      data: null
+    }
+  }
+}
+
+// export async function cadastrarCliente(cliente) {
+//   try {
+//     const response = await fetch(`${urlApi}/api/Cliente`, {
+//       method: "POST",
+//       headers: {
+//         "Content-Type": "application/json"
+//       },
+//       body: JSON.stringify(cliente)
+//     });
+
+//     const contentType = response.headers.get("content-type");
+
+//     let dados;
+//     if (contentType && contentType.includes("application/json")) {
+//       dados = await response.json();
+//     } else {
+//       dados = await response.text(); 
+//     }
+
+//     return {
+//       status: response.status,
+//       data: dados
+//     };
+//   } catch (err) {
+//     return {
+//       status: 500,
+//       data: err.message
+//     };
+//   }
+// }
+
 export async function cadastrarCliente(cliente) {
   try {
     const response = await fetch(`${urlApi}/api/Cliente`, {
@@ -10,23 +64,14 @@ export async function cadastrarCliente(cliente) {
       body: JSON.stringify(cliente)
     });
 
-    const contentType = response.headers.get("content-type");
-
-    let dados;
-    if (contentType && contentType.includes("application/json")) {
-      dados = await response.json();
-    } else {
-      dados = await response.text(); 
-    }
-
-    return {
+    let dados = await response.json();
+    return{
       status: response.status,
       data: dados
-    };
+    }
   } catch (err) {
-    console.error("Erro de rede ou servidor:", err);
     return {
-      status: 0,
+      status: 500,
       data: err.message
     };
   }
